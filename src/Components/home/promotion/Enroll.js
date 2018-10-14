4b3b0acc72e7b4635e9986bdf6cb689134f5d716
Graham Cottridge <codinggc@gmail.com>
2018-10-14 22:48:35 +0100

    Create enroll component

---
+++ src/Components/home/promotion/Enroll.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+
+class Enroll extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Enroll;
