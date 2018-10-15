f02671d15d11fc615ea9856255fc3ba16dea4791
Graham Cottridge <codinggc@gmail.com>
2018-10-15 23:45:48 +0100

    Set up enroll form

--- src/Components/home/promotion/Enroll.js
+++ src/Components/home/promotion/Enroll.js
@@ -2,9 +2,49 @@ import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 
 class Enroll extends Component {
+
+  state = {
+      formError: false,
+      formSuccess: '',
+      formdata: {
+        email: {
+          element: 'input',
+          value: '',
+          config: {
+            name: 'email_input',
+            type: 'email',
+            placeholder: 'Enter your email'
+          },
+          validation: {
+            required: true,
+            email: true
+          },
+          valid: false,
+          validationMessage: ''
+        }
+      }
+    }
+
+  submitForm() {
+
+  }
+
   render() {
     return (
-      <div></div>
+      <Fade>
+      <div className="enroll-wrapper">
+        <form onSubmit={ (event) => this.submitForm(event)}>
+          <div className="enroll-title">
+            Enter your email
+          </div>
+          <div className="enroll-input">
+
+          </div>
+        </form>
+
+      </div>
+
+      </Fade>
     );
   }
 }
--- src/Components/home/promotion/index.js
+++ src/Components/home/promotion/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PromotionAnimation from './Animation';
+import Enroll from './Enroll';
 
 const Promotion = () => {
     return (
       <div className="promotion-wrapper" style={{background: '#ffffff'}}>
         <div className="container">
           <PromotionAnimation/>
+          <Enroll/>
         </div>
       </div>
     );
