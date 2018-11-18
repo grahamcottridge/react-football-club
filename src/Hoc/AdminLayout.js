4c68c2ddcd4472266db2070686005d85e7b15ed7
Graham Cottridge <codinggc@gmail.com>
2018-11-18 23:37:16 +0000

    Create dashboard nav

--- src/Hoc/AdminLayout.js
+++ src/Hoc/AdminLayout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AdminNav from '../Components/admin/nav/AdminNav';
 
 const AdminLayout = (props) => {
     return (
       <div className="admin-container">
         <div className="admin-left-nav">
-          nav
+          <AdminNav/>
         </div>
         <div className="admin-right">
           {props.children}
